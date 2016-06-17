c027bd22d04da683955d3286c5a667acf115dfd8
lzancan <luciano-zancan@hotmail.com>
2016-06-17 12:49:57 -0300

    add os bgl

---
+++ t2fs/include/util.h
@@ -0,0 +1,17 @@
+#ifndef __util_h__
+#define __util_h__
+
+/*
+retorna a string "from" de acordo com o tamanho a ser copiado.
+*/
+char* copy_string_size (char* from, int size);
+
+
+
+
+
+
+
+
+
+#endif
